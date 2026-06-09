f973eae1ed69648518459b21804e06d4d8017892
hadiamughal8 <nosheenkhatib79@gmail.com>
2026-06-09 23:16:47 +0500

    course class

---
+++ src/course.h
@@ -0,0 +1,209 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class CapacityExceededException
+{
+public:
+    const char* what() const
+    {
+        return "Course capacity exceeded!";
+    }
+};
+class Course
+{
+private:
+    string courseCode;
+    string courseName;
+    int creditHours;
+    int maxCapacity;
+    int enrolledCount;
+
+public:
+    Course()
+    {
+        courseCode = "";
+        courseName = "";
+        creditHours = 0;
+        maxCapacity = 0;
+        enrolledCount = 0;
+    }
+
+    Course(string code, string name, int credits, int capacity)
+    {
+        courseCode = code;
+        courseName = name;
+        creditHours = credits;
+        maxCapacity = capacity;
+        enrolledCount = 0;
+    }
+
+    
+    string getCourseCode() const
+    {
+        return courseCode;
+    }
+
+    string getCourseName() const
+    {
+        return courseName;
+    }
+
+    int getCreditHours() const
+    {
+        return creditHours;
+    }
+
+    int getMaxCapacity() const
+    {
+        return maxCapacity;
+    }
+
+    int getEnrolledCount() const
+    {
+        return enrolledCount;
+    }
+
+    
+    void setCourseName(string name)
+    {
+        courseName = name;
+    }
+
+    void enrollStudent()
+    {
+        if(enrolledCount >= maxCapacity)
+        {
+            throw CapacityExceededException();
+        }
+
+        enrolledCount++;
+    }
+
+    
+    bool operator==(const Course& other)
+    {
+        return courseCode == other.courseCode;
+    }
+
+
+    friend ostream& operator<<(ostream& out, const Course& c)
+    {
+        out << "Course Code: " << c.courseCode << endl;
+        out << "Course Name: " << c.courseName << endl;
+        out << "Credit Hours: " << c.creditHours << endl;
+        out << "Capacity: "
+            << c.enrolledCount << "/"
+            << c.maxCapacity << endl;
+
+        return out;
+    }
+};
+class Enrollment
+{
+private:
+    string studentName;
+    Course* course;
+    string enrollmentDate;
+    string grade;
+
+public:
+    Enrollment()
+    {
+        course = NULL;
+    }
+
+    Enrollment(string s, Course* c,
+               string date, string g)
+    {
+        studentName = s;
+        course = c;
+        enrollmentDate = date;
+        grade = g;
+    }
+
+    void display()
+    {
+        cout << "\nStudent: "
+             << studentName << endl;
+
+        cout << "Course: "
+             << course->getCourseName() << endl;
+
+        cout << "Date: "
+             << enrollmentDate << endl;
+
+        cout << "Grade: "
+             << grade << endl;
+    }
+};
+class WaitingList
+{
+private:
+    string students[20];
+    int count;
+
+public:
+    WaitingList()
+    {
+        count = 0;
+    }
+
+    void addStudent(string name)
+    {
+        students[count++] = name;
+    }
+
+    WaitingList operator+(const WaitingList& other)
+    {
+        WaitingList temp;
+
+        for(int i=0;i<count;i++)
+            temp.students[temp.count++] = students[i];
+
+        for(int i=0;i<other.count;i++)
+            temp.students[temp.count++] =
+                other.students[i];
+
+        return temp;
+    }
+
+    void display()
+    {
+        for(int i=0;i<count;i++)
+            cout << students[i] << endl;
+    }
+};
+int main()
+{
+    Course c1("CS101",
+              "OOP",
+              3,
+              2);
+
+    try
+    {
+        c1.enrollStudent();
+        c1.enrollStudent();
+
+        c1.enrollStudent(); 
+    }
+    catch(CapacityExceededException e)
+    {
+        cout << e.what() << endl;
+    }
+
+    cout << c1 << endl;
+
+    Course c2("CS101",
+              "Programming",
+              3,
+              50);
+
+    if(c1 == c2)
+    {
+        cout << "Same Course Code\n";
+    }
+
+    return 0;
+}
